a68636986983c8d226e299643ddd6f980ae59d20
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-02-02 10:35:05 +0100

    Clean Xcode project template

--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -1,25 +1,5 @@
-//
-//  ViewController.swift
-//  Movies
-//
-//  Created by Tomáš Znamenáček on 02/02/2017.
-//  Copyright © 2017 Tomáš Znamenáček. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
