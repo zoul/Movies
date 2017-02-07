f643edec006ddf9ca4bd8a7be0209fa44abaaead
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-02-07 16:45:34 +0100

    Improve navigation bar appearance

--- Movies/AppDelegate.swift
+++ Movies/AppDelegate.swift
@@ -13,6 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let rootVC = MovieListController()
         let navigationVC = UINavigationController(rootViewController: rootVC)
 
+        let navBar = navigationVC.navigationBar
+        navBar.barTintColor = UIColor(colorLiteralRed: 0, green: 175/255, blue: 1, alpha: 0)
+        navBar.tintColor = UIColor.white
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        navBar.isTranslucent = false
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationVC
         window?.makeKeyAndVisible()
