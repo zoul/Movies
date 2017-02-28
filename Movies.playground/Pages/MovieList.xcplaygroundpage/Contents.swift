2367df595e0161766d6a3bb8c331637f00fa0056
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-02-28 17:56:38 +0100

    Add sample results to the MovieList playground

--- Movies.playground/Pages/MovieList.xcplaygroundpage/Contents.swift
+++ Movies.playground/Pages/MovieList.xcplaygroundpage/Contents.swift
@@ -4,5 +4,8 @@ import MoviesUI
 import PlaygroundSupport
 
 let listVC = MovieListController()
+listVC.loadOneMorePage = { callback in
+    callback(MovieListPage.template.results)
+}
 
 PlaygroundPage.current.liveView = listVC
